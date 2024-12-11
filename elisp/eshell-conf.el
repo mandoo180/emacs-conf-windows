1b13d7f97e3cb3a17fd4c692f69d5aec47c63a97
Kyeong Soo Choi <mandoo180@gmail.com>
2024-12-11 18:00:44 +0900

    fix: remove some eshell config codes

--- elisp/eshell-conf.el
+++ elisp/eshell-conf.el
@@ -1,97 +1,42 @@
-(defun curr-dir-git-branch-string (pwd)
-  "Returns current git branch as a string, or the empty string if
-PWD is not in a git repo (or the git command is not found)."
+(defun eshell-new ()
+  "Open a new insctance of eshell."
   (interactive)
-  (when (and (not (file-remote-p pwd))
-             (eshell-search-path "git")
-             (locate-dominating-file pwd ".git"))
-    (let* ((git-url (shell-command-to-string "git config --get remote.origin.url"))
-           (git-repo (file-name-base (s-trim git-url)))
-           (git-output (shell-command-to-string (concat "git rev-parse --abbrev-ref HEAD")))
-           (git-branch (s-trim git-output))
-           (git-icon  "\xe0a0")
-           (git-icon2 (propertize "\xf020" 'face `(:family "octicons"))))
-      (concat git-repo " " git-icon2 " " git-branch))))
+  (eshell 'N))
 
-(defun pwd-replace-home (pwd)
-  "Replace home in PWD with tilde (~) character."
+;; toggle eshell prompt by kyeongsoo
+;; debugging with C-h e (message buffer on the side)
+
+(defun eshell-below()
+  "Split window below with eshell buffer."
   (interactive)
-  (let* ((home (expand-file-name (getenv "HOME")))
-         (home-len (length home)))
-    (if (and
-         (>= (length pwd) home-len)
-         (equal home (substring pwd 0 home-len)))
-        (concat "~" (substring pwd home-len))
-      pwd)))
+  (let* ((height (/ (window-total-height) 3)))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell)))
 
-
-(defun pwd-shorten-dirs (pwd)
-  "Shorten all directory names in PWD except the last two."
-  (let ((p-lst (split-string pwd "/")))
-    (if (> (length p-lst) 2)
-        (concat
-         (mapconcat (lambda (elm) (if (zerop (length elm)) ""
-                                    (substring elm 0 1)))
-                    (butlast p-lst 2)
-                    "/")
-         "/"
-         (mapconcat (lambda (elm) elm)
-                    (last p-lst 2)
-                    "/"))
-      pwd)))  ;; Otherwise, we just return the PWD
-
-(defun python-prompt ()
-  "Returns a string (may be empty) based on the current Python
-   Virtual Environment. Assuming the M-x command: `pyenv-mode-set'
-   has been called."
-  (when (fboundp #'pyenv-mode-version)
-    (let ((venv (pyenv-mode-version)))
-      (when venv
-        (concat
-         (propertize "\xe928" 'face `(:family "alltheicons"))
-         (pyenv-mode-version))))))
-
-(defun split-directory-prompt (directory)
-  (if (string-match-p ".*/.*" directory)
-      (list (file-name-directory directory) (file-name-base directory))
-    (list "" directory)))
-
-(defun eshell/eshell-local-prompt-function ()
-  "A prompt for eshell that works locally (in that is assumes
-that it could run certain commands) in order to make a prettier,
-more-helpful local prompt."
+(defun toggle-eshell()
+  "Toggle split window with eshell buffer below."
   (interactive)
-  (let* ((pwd        (eshell/pwd))
-         (directory (split-directory-prompt
-                     (pwd-shorten-dirs
-                      (pwd-replace-home pwd))))
-         (parent (car directory))
-         (name   (cadr directory))
-         (branch (curr-dir-git-branch-string pwd))
-         (python (when (not (file-remote-p pwd)) (python-prompt)))
+  (let* ((w (window-in-direction 'below)))
+    (with-current-buffer (window-buffer w)
+      (if (eq major-mode 'eshell-mode)
+          (delete-window w)
+        (eshell-below)))))
 
-         (dark-env (eq 'dark (frame-parameter nil 'background-mode)))
-         (for-bars                 `(:weight bold))
-         (for-parent  (if dark-env `(:foreground "dark orange") `(:foreground "blue")))
-         (for-dir     (if dark-env `(:foreground "orange" :weight bold)
-                        `(:foreground "blue" :weight bold)))
-         (for-git                  `(:foreground "green"))
-         (for-python               `(:foreground "#5555FF")))
+;; no use but think it's good to know
+(defun eshell-buffers()
+  "Returns eshell buffer list."
+  (delq nil (mapcar (lambda (buf)
+                      (with-current-buffer buf
+                        (if (eq major-mode 'eshell-mode)
+                            buf)))
+                    (buffer-list))))
 
-    (concat
-     ;; (propertize "K " 'face for-bars)
-     (propertize parent 'face for-parent)
-     (propertize name 'face for-dir)
-     (when branch
-       (concat (propertize " ── "    'face for-bars)
-               (propertize branch   'face for-git)))
-     (when python
-       (concat (propertize " ── " 'face for-bars)
-               (propertize python 'face for-python)))
-     (propertize "\n"     'face for-bars)
-     (propertize (if (= (user-uid) 0) " #" " $") 'face `(:weight ultra-bold))
-     ;; (propertize " └→" 'face (if (= (user-uid) 0) `(:weight ultra-bold :foreground "red") `(:weight ultra-bold)))
-     (propertize " "    'face `(:weight bold)))))
+(defun handle-eshell-exit()
+  "Deletes the window when eshell buffer exits."
+  (if (> (count-windows 1))
+      (delete-window)))
+
 
 (defun k/configure-eshell ()
   ;; Save command history when commands are entered
@@ -125,38 +70,13 @@ more-helpful local prompt."
               (eshell/alias "ll" "ls -ahl $1")
               (eshell/alias "ff" "find-file $1")
               (eshell/alias "emacs" "find-file $1")
-              (eshell/alias "ffo" "find-file-other-window $1"))))
-
-(use-package eshell-git-prompt
-  :after eshell)
+              (eshell/alias "ffo" "find-file-other-window $1")))
+  (add-hook 'eshell-exit-hook  #'handle-eshell-exit))
 
 (use-package eshell
-  :hook (eshell-first-time-mode . k/configure-eshell))
+  :hook
+  (eshell-first-time-mode . k/configure-eshell))
 
-(defun eshell/clear ()
-  "Clear the eshell buffer."
-  (let ((inhibit-read-only t))
-    (erase-buffer)
-    (eshell-send-input)))
-
-(defun eshell/gst (&rest args)
-  (magit-status (pop args) nil)
-  (eshell/echo)) ;; the echo command suppresses output
-
-(defun eshell-new ()
-  "Open a new insctance of eshell."
-  (interactive)
-  (eshell 'N))
-
-(defun eshell-here ()
-  "Opens up a new shell in the directory associated with the current
-buffers's file. the eshell is renamed to match that directory to make
-multiple eshell windows easier."
-  (interactive)
-  (let* ((height (/ (window-total-height) 3)))
-    (split-window-vertically (- height))
-    (other-window 1)
-    (eshell "new")
-    (eshell-send-input)))
+(global-set-key (kbd "C-`") #'toggle-eshell)
 
 (provide 'eshell-conf)
