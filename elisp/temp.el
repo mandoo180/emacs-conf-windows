d19e0ab32aaeca7dc20b17af7d5b2e65563eb993
Kyeong Soo Choi <mandoo180@gmail.com>
2025-03-04 18:07:44 +0900

    fix: change consult keybindings

--- elisp/temp.el
+++ elisp/temp.el
@@ -37,4 +37,28 @@
   (setq gptel-api-key (getenv "OPENAI_API_KEY"))
   (setq gptel-default-mode 'org-mode))
 
+(defun dz-copy-to-html-cx ()
+  "Copy html file to CX directory."
+  (interactive)
+  (let* ((srcfile (f-filename (buffer-file-name)))
+         (src (buffer-file-name))
+         (destbase "C:/Users/douzone/Projects/douzone/dews-web/repository/view/html/CX/")
+         (dest (expand-file-name srcfile destbase)))
+    (message "src: %s, dest: %s" src dest)
+    (copy-file src dest t)))
+
+(package-vc-install '(ultra-scroll :vc-backend Git :url  "https://github.com/jdtsmith/ultra-scroll"))
+(setq scroll-conservatively 101 ; important!
+      scroll-margin 0)
+(ultra-scroll-mode 1)
+
+(use-package ultra-scroll
+  ;:load-path "~/code/emacs/ultra-scroll" ; if you git clone'd instead of package-vc-install
+  :init
+  (setq scroll-conservatively 101 ; important!
+        scroll-margin 0) 
+  :config
+  (ultra-scroll-mode 1))
+
 (provide 'temp)
+
